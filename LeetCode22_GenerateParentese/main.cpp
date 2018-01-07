0704dd2dd75072923c7b92e2284eb741f5daceb9
Rui <rui@dhcp154.merusouth116.iit.edu>
2018-01-06 23:41:09 -0600

    first commit

--- LeetCode22_GenerateParentese/main.cpp
+++ LeetCode22_GenerateParentese/main.cpp
@@ -13,3 +13,28 @@ int main(int argc, const char * argv[]) {
     std::cout << "Hello, World!\n";
     return 0;
 }
+
+
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> res;
+        generateParenthesisDFS(n, n,"", res);
+        return res;
+        
+    }
+    
+    void generateParenthesisDFS(int left, int right, string out, vector<string> &res)
+    {
+        if(left > right)
+            return;
+        if(left == 0 && right == 0)
+            res.push_back(out);
+        else{
+            if(left > 0)
+                generateParenthesisDFS(left - 1, right, out + "(", res);
+            if(right > 0)
+                generateParenthesisDFS(left, right - 1, out + ")", res);
+        }
+    }
+};
